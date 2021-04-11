4f996e26ac2c0b6487a0b5048b15e018f1fbf638
Drew Finnis <drewfinnis@gmail.com>
2021-04-11 14:41:52 +0200

    feature/main and src/

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	multilayer "Multilayer_Perceptron/src"
+)
+
+func main() {
+	multilayer.MultilayerPerceptron()
+}
+
+// ## To run enter either command:
+// go run main.go
+// go build; ./Multilayer_Perceptron
---
+++ src/multilayerPerceptron.go
@@ -0,0 +1,10 @@
+package multilayer
+
+import (
+	"fmt"
+)
+
+// MultilayerPerceptron is the main and only exposed function
+func MultilayerPerceptron() {
+	fmt.Println("Oh HI!!!!!!!!!!!!!!!!!!!!!!!!")
+}

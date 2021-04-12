88499f072d0bcfcf59b51c52d8e4ff3a34076242
Drew Finnis <drewfinnis@gmail.com>
2021-04-12 15:00:39 +0200

    feature/WIP neural network struct neuron

--- src/multilayerPerceptron.go
+++ src/multilayerPerceptron.go
@@ -13,7 +13,7 @@ type neuron struct {
 
 type layer struct {
 	length uint8
-	// neurons []neuron
+	neurons []neuron
 	// activation function_pointer (sigmoid / softmax)
 }
 
@@ -33,9 +33,22 @@ func MultilayerPerceptron() {
 	nn := neuralNetwork{}
 	nn.learningRate = 0.01
 
-	input := layer{16}
-	hidden := layer{16}
-	output := layer{2}
+	// n := neuron{42} // init random
+
+	// list := neuron{}
+	var hiddenNeurons []neuron
+	for i := 0; i < 16; i++ {
+		hiddenNeurons = append(hiddenNeurons, neuron{42})
+	}
+
+	var outputNeurons []neuron
+	for i := 0; i < 2; i++ {
+		outputNeurons = append(outputNeurons, neuron{1})
+	}
+
+	input := layer{16, hiddenNeurons}
+	hidden := layer{16, hiddenNeurons}
+	output := layer{2, outputNeurons}
 
 	nn.layers = append(nn.layers, input)
 	nn.layers = append(nn.layers, hidden)

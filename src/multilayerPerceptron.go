34cf0f82e31e376f49220db56524c69468807480
Drew Finnis <drewfinnis@gmail.com>
2021-04-12 19:57:26 +0200

    feature/newNeuron & newLayer functions

--- src/multilayerPerceptron.go
+++ src/multilayerPerceptron.go
@@ -7,8 +7,8 @@ import (
 
 type neuron struct {
 	value float64
-	// weights []float64
-	// bias float64
+	bias float64
+	weights []float64
 }
 
 type layer struct {
@@ -26,6 +26,24 @@ type neuralNetwork struct {
 // 	return nn.learningRate, 1
 // }
 
+
+func newNeuron(value float64) neuron {
+	return neuron{
+		value:				value,
+	}
+}
+
+func newLayer(length uint8) layer {
+	var neurons []neuron
+	var i uint8
+	for i = 0; i < length; i++ {
+		neurons = append(neurons, newNeuron(42))
+	}
+	l := layer{length, neurons}
+	return l
+}
+
+
 // MultilayerPerceptron is the main and only exposed function
 func MultilayerPerceptron() {
 	fmt.Println("Oh HI!!!!!!!!!!!!!!!!!!!!!!!!")
@@ -33,27 +51,32 @@ func MultilayerPerceptron() {
 	nn := neuralNetwork{}
 	nn.learningRate = 0.01
 
-	// n := neuron{42} // init random
-
 	// list := neuron{}
-	var hiddenNeurons []neuron
-	for i := 0; i < 16; i++ {
-		hiddenNeurons = append(hiddenNeurons, neuron{42})
-	}
+	// var hiddenNeurons []neuron
+	// for i := 0; i < 16; i++ {
+	// 	// hiddenNeurons = append(hiddenNeurons, neuron{42})
+	// 	hiddenNeurons = append(hiddenNeurons, newNeuron(42))
+	// }
 
-	var outputNeurons []neuron
-	for i := 0; i < 2; i++ {
-		outputNeurons = append(outputNeurons, neuron{1})
-	}
+	// var outputNeurons []neuron
+	// for i := 0; i < 2; i++ {
+	// 	outputNeurons = append(outputNeurons, newNeuron(1))
+	// }
 
-	input := layer{16, hiddenNeurons}
-	hidden := layer{16, hiddenNeurons}
-	output := layer{2, outputNeurons}
+	// input := layer{16, hiddenNeurons}
+	// hidden := layer{16, hiddenNeurons}
+	// output := layer{2, outputNeurons}
 
-	nn.layers = append(nn.layers, input)
-	nn.layers = append(nn.layers, hidden)
-	nn.layers = append(nn.layers, hidden)
-	nn.layers = append(nn.layers, output)
+	// nn.layers = append(nn.layers, input)
+	// nn.layers = append(nn.layers, hidden)
+	// nn.layers = append(nn.layers, hidden)
+	// nn.layers = append(nn.layers, output)
+
+	nn.layers = append(nn.layers, newLayer(16))
+	nn.layers = append(nn.layers, newLayer(16))
+	nn.layers = append(nn.layers, newLayer(16))
+	nn.layers = append(nn.layers, newLayer(2))
+
 
 	// fmt.Println(nn.config())
 	fmt.Println(nn.learningRate)

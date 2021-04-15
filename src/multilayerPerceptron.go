d75271d3fbfb15fea111560bbf06c36b258827db
Drew Finnis <drewfinnis@gmail.com>
2021-04-15 10:16:31 +0200

    feature/softmax output layer

--- src/multilayerPerceptron.go
+++ src/multilayerPerceptron.go
@@ -10,30 +10,6 @@ import (
 
 type activationFunc func(nn neuralNetwork, layer int)
 
-func sigmoid(z float64) float64 {
-	return 1/(1+math.Exp(-z))
-}
-
-func softmax(x []float64) []float64 {
-	var max float64 = x[0]
-	for _, n := range x {
-		max = math.Max(max, n)
-	}
-
-	a := make([]float64, len(x))
-
-	var sum float64 = 0
-	for i, n := range x {
-		a[i] -= math.Exp(n - max)
-		sum += a[i]
-	}
-
-	for i, n := range a {
-		a[i] = n / sum
-	}
-	return a
-}
-
 type neuron struct {
 	bias float64
 	weights []float64
@@ -88,15 +64,55 @@ func buildNN(architecture []int) neuralNetwork {
 	nn.architecture = architecture
 	nn.learningRate = 0.01
 
-	for layer := 0; layer < len(architecture); layer++ {
+	var layer int
+	for layer = 0; layer < len(architecture); layer++ {
 		nn.layers = append(nn.layers, newLayer(layer, nn))
 	}
+	nn.layers[layer - 1].activation = softmaxLayer
 	return nn
+}
+
+
+func sigmoid(z float64) float64 {
+	return 1/(1+math.Exp(-z))
 }
 
 func sigmoidLayer(nn neuralNetwork, layer int) {
 	for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
 		nn.layers[layer].neurons[neuron].output = sigmoid(nn.layers[layer].neurons[neuron].value)
+	}
+}
+
+func softmax(values []float64) []float64 {
+	max := values[0]
+	for _, value := range values {
+		max = math.Max(max, value)
+	}
+
+	outputs := make([]float64, len(values))
+	var sum float64
+	for i, value := range values {
+		outputs[i] -= math.Exp(value - max)
+		sum += outputs[i]
+	}
+	for i, output := range outputs {
+		outputs[i] = output / sum
+	}
+	return outputs
+}
+
+func softmaxLayer(nn neuralNetwork, layer int) {
+	// fmt.Printf("layer: %v\n", layer) ////////////////////////////
+	var values []float64
+	for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
+		// fmt.Printf("neuron: %v\n", neuron) ///////////////////////////
+		values = append(values, nn.layers[layer].neurons[neuron].value)
+	}
+	// fmt.Printf("values: %v\n", values) ///////////////////////////
+	ouput := softmax(values)
+	// fmt.Printf("ouput: %v\n", ouput) ///////////////////////////
+	for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
+		nn.layers[layer].neurons[neuron].output = ouput[neuron]
 	}
 }
 
@@ -126,7 +142,7 @@ func MultilayerPerceptron() {
 	rand.Seed(time.Now().UnixNano())
 
 	// architecture := []int {16, 16, 16, 16, 2}
-	architecture := []int {2, 2, 2, 2} // test architecture
+	architecture := []int {2, 2, 2, 2} // test architecture ////
 	nn := buildNN(architecture)
 
 	feedforward(nn)

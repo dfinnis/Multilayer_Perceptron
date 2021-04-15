34905fbd0127ec62c81f545f4a2b42e1ed125da8
Drew Finnis <drewfinnis@gmail.com>
2021-04-15 10:28:49 +0200

    refacto/split network.go

--- src/multilayerPerceptron.go
+++ src/multilayerPerceptron.go
@@ -2,70 +2,10 @@ package multilayer
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
 	"time"
 	// "gonum/mat" // matrix linear algebra // gonum.org/v1/gonum/mat
 )
-
-type activationFunc func(nn neuralNetwork, layer int)
-
-type neuron struct {
-	bias float64
-	weights []float64
-	value float64 // before activation func
-	output float64 // after activation func
-}
-
-type layer struct {
-	// label string
-	// length int
-	neurons []neuron
-	activation activationFunc // input & hidden = sigmoid, output = softmax
-}
-
-type neuralNetwork struct {
-	architecture []int
-	layers []layer
-	learningRate float64
-}
-
-func newNeuron(currentLayer int, nn neuralNetwork) neuron {
-	var weights []float64
-	if currentLayer > 0 {
-		for i := 0; i < nn.architecture[currentLayer - 1]; i++ {
-			weights = append(weights, rand.Float64() * math.Sqrt(2/float64(nn.architecture[currentLayer - 1])))
-		}
-	}
-	return neuron {
-		weights:			weights,
-	}
-}
-
-func newLayer(currentLayer int, nn neuralNetwork) layer {
-	var neurons []neuron
-	for i := 0; i < nn.architecture[currentLayer]; i++ {
-		neurons = append(neurons, newNeuron(currentLayer, nn))
-	}
-	return layer {
-		// length:				nn.architecture[currentLayer], ////////////
-		neurons:			neurons,
-		activation:			sigmoidLayer,
-	}
-}
-
-func buildNN(architecture []int) neuralNetwork {
-	nn := neuralNetwork{}
-	nn.architecture = architecture
-	nn.learningRate = 0.01
-
-	var layer int
-	for layer = 0; layer < len(architecture); layer++ {
-		nn.layers = append(nn.layers, newLayer(layer, nn))
-	}
-	nn.layers[layer - 1].activation = softmaxLayer
-	return nn
-}
 
 func feedforward(nn neuralNetwork) {
 	nn.layers[0].neurons[0].output = 0.42 // input layer //////////////////////////////////////////////
---
+++ src/network.go
@@ -0,0 +1,65 @@
+package multilayer
+
+import (
+	"math"
+	"math/rand"
+)
+
+type activationFunc func(nn neuralNetwork, layer int)
+
+type neuron struct {
+	bias float64
+	weights []float64
+	value float64 // before activation func
+	output float64 // after activation func
+}
+
+type layer struct {
+	// label string
+	// length int
+	neurons []neuron
+	activation activationFunc // input & hidden = sigmoid, output = softmax
+}
+
+type neuralNetwork struct {
+	architecture []int
+	layers []layer
+	learningRate float64
+}
+
+func newNeuron(currentLayer int, nn neuralNetwork) neuron {
+	var weights []float64
+	if currentLayer > 0 {
+		for i := 0; i < nn.architecture[currentLayer - 1]; i++ {
+			weights = append(weights, rand.Float64() * math.Sqrt(2/float64(nn.architecture[currentLayer - 1])))
+		}
+	}
+	return neuron {
+		weights:			weights,
+	}
+}
+
+func newLayer(currentLayer int, nn neuralNetwork) layer {
+	var neurons []neuron
+	for i := 0; i < nn.architecture[currentLayer]; i++ {
+		neurons = append(neurons, newNeuron(currentLayer, nn))
+	}
+	return layer {
+		// length:				nn.architecture[currentLayer], ////////////
+		neurons:			neurons,
+		activation:			sigmoidLayer,
+	}
+}
+
+func buildNN(architecture []int) neuralNetwork {
+	nn := neuralNetwork{}
+	nn.architecture = architecture
+	nn.learningRate = 0.01
+
+	var layer int
+	for layer = 0; layer < len(architecture); layer++ {
+		nn.layers = append(nn.layers, newLayer(layer, nn))
+	}
+	nn.layers[layer - 1].activation = softmaxLayer
+	return nn
+}

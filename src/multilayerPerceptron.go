8b7be6cc581be2714f201237fdc3ef9e4863a2dd
Drew Finnis <drewfinnis@gmail.com>
2021-04-19 19:03:15 +0200

    feature/WIP backpropLayer, works for last layer

--- src/multilayerPerceptron.go
+++ src/multilayerPerceptron.go
@@ -54,6 +54,60 @@ func compute_loss_prime(outputs [][]float64, y [][]float64) (d_losses [][]float6
 	return
 }
 
+func backpropLayer(nn neuralNetwork, output, y [][]float64, layer int, d_A [][]float64) {
+	var z [][]float64
+	for sample, _ := range output {
+		var layer_value []float64
+		for _, neuron := range nn.layers[layer].neurons {
+			layer_value = append(layer_value, neuron.z[sample])
+		}
+		z = append(z, layer_value)
+	}
+	prime := softmax_prime(z) ///////// activation for layer !!!!!!!!!!!!!!!!!!
+
+	var d_z [][]float64
+	for i, sample := range prime {
+		var layer_d []float64
+		for j, value := range sample {
+			layer_d = append(layer_d, d_A[i][j] * value)
+		}
+		d_z = append(d_z, layer_d)
+	}
+
+	var layerOutputs [][]float64
+	for sample, _ := range prime {
+		var layerOutput []float64
+		for neuron := 0; neuron < nn.architecture[layer - 1]; neuron++ {
+			layerOutput = append(layerOutput, nn.layers[layer - 1].neurons[neuron].outputs[sample])
+		}
+		layerOutputs = append(layerOutputs, layerOutput)
+	}
+
+	d_weights := multiply(transpose(d_z), layerOutputs)
+
+	d_bias := make([]float64, nn.architecture[layer])
+	for _, sample := range d_z {
+		for neuron, _ := range nn.layers[layer].neurons {
+			d_bias[neuron] += sample[neuron]
+		}
+	}
+
+	var weights [][]float64
+	for _, neuron := range nn.layers[layer].neurons {
+		var weightLayer []float64
+		for _, weight := range neuron.weights {
+			weightLayer = append(weightLayer, weight)
+		}
+		weights = append(weights, weightLayer)
+	}
+	// d_aPrevious := multiply2(d_z, weights)
+
+	fmt.Printf("d_bias): %v\n", d_bias) ////////////
+	fmt.Printf("d_weights): %v\n", d_weights) ////////////
+
+	// return d_aPrevious
+}
+
 func backprop(nn neuralNetwork, output, y [][]float64) {
 	d_A4 := compute_loss_prime(output, y)
 
@@ -127,6 +181,11 @@ func backprop(nn neuralNetwork, output, y [][]float64) {
 
 	fmt.Printf("d_bias4): %v\n", d_bias4) ////////////
 	fmt.Printf("d_weights4): %v\n", d_weights4) ////////////
+
+	for layer := len(nn.architecture) - 1; layer >= 0; layer-- {
+		backpropLayer(nn, output, y, layer, d_A4)
+		break ///////////////////
+	}
 }
 
 func train(nn neuralNetwork, train_set [][]float64, test_set [][]float64) {

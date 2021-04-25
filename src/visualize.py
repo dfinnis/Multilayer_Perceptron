2e690fcc34328d185fa04ecedff0f5958e47bc90
Drew Finnis <drewfinnis@gmail.com>
2021-04-25 13:50:01 +0200

    fix/vizualizer string vs float error

--- src/visualize.py
+++ src/visualize.py
@@ -2,22 +2,25 @@ import argparse
 import matplotlib.pyplot as plt
 
 def error_exit(err_msg):
-    print('Error: {}' .format(err_msg))
-    sys.exit()
+	print('Error: {}' .format(err_msg))
+	sys.exit()
 
 def parse_arg():
-    my_parser = argparse.ArgumentParser(description="Visualize loss over training period")
-    my_parser.add_argument('Loss',
-                       metavar='loss',
-                       type=str,
-                       help='loss')
-    args = my_parser.parse_args()
-    loss = args.Loss
-    return loss
+	my_parser = argparse.ArgumentParser(description="Visualize loss over training period")
+	my_parser.add_argument('Loss',
+					   metavar='loss',
+					   type=str,
+					   help='loss')
+	args = my_parser.parse_args()
+	loss = args.Loss
+	return loss
 
 def Convert(string):
-    li = list(string.split(" "))
-    return li
+	floats=[]
+	strings = string.split(" ")
+	for element in strings:
+		floats.append(float(element))
+	return floats
 
 def visualize(loss):
 	loss = Convert(loss)
@@ -31,7 +34,6 @@ def visualize(loss):
 	plt.title('Loss over training period')
 	plt.xlabel('Epoch')
 	plt.ylabel('Loss')
-	plt.yticks([])
 	plt.show()
 
 def main():

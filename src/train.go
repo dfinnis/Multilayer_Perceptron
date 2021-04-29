16aa63562218f9a72377709893af84452546eaa7
Drew Finnis <drewfinnis@gmail.com>
2021-04-29 14:58:40 +0200

    refacto/train to subfunctions

--- src/train.go
+++ src/train.go
@@ -4,36 +4,50 @@ import (
 	"fmt"
 )
 
+// setInput loads one samples data to the input layer
+func setInput(nn neuralNetwork, inputs [][]float64, sample int) {
+	for i := 0; i < len(inputs[0]); i++ {
+		nn.layers[0].neurons[i].output = inputs[sample][i]
+		nn.layers[0].neurons[i].outputs = append(nn.layers[0].neurons[i].outputs, inputs[sample][i])
+	}
+}
+
+// feedforwardSample runs one sample through the network
+func feedforwardSample(nn neuralNetwork) {
+	for layer := 1; layer < len(nn.architecture); layer++ {
+		for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
+			perceptron := nn.layers[layer].neurons[neuron] // rm for speed? just for human reading
+			var weightedSum float64
+			for weight := 0; weight < len(perceptron.weights); weight++ {
+				weightedSum += nn.layers[layer-1].neurons[weight].output * perceptron.weights[weight]
+			}
+			nn.layers[layer].neurons[neuron].value = weightedSum + perceptron.bias
+			nn.layers[layer].neurons[neuron].z = append(nn.layers[layer].neurons[neuron].z, nn.layers[layer].neurons[neuron].value)
+		}
+		nn.layers[layer].activation(nn, layer)
+	}
+}
+
+// getOutput reads the output layer
+func getOutput(nn neuralNetwork) []float64 {
+	var output []float64
+	for neuron := 0; neuron < nn.architecture[len(nn.architecture)-1]; neuron++ {
+		output = append(output, nn.layers[len(nn.architecture)-1].neurons[neuron].output)
+	}
+	return output
+}
+
+// feedforward inputs samples & reads the output
 func feedforward(nn neuralNetwork, inputs [][]float64) (outputs [][]float64) {
 	for sample := 0; sample < len(inputs); sample++ {
-		// Input
-		for i := 0; i < len(inputs[0]); i++ {
-			nn.layers[0].neurons[i].output = inputs[sample][i]
-			nn.layers[0].neurons[i].outputs = append(nn.layers[0].neurons[i].outputs, inputs[sample][i])
-		}
-		// Feedforward
-		for layer := 1; layer < len(nn.architecture); layer++ {
-			for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
-				perceptron := nn.layers[layer].neurons[neuron] // rm for speed? just for human reading
-				var weightedSum float64
-				for weight := 0; weight < len(perceptron.weights); weight++ {
-					weightedSum += nn.layers[layer-1].neurons[weight].output * perceptron.weights[weight]
-				}
-				nn.layers[layer].neurons[neuron].value = weightedSum + perceptron.bias
-				nn.layers[layer].neurons[neuron].z = append(nn.layers[layer].neurons[neuron].z, nn.layers[layer].neurons[neuron].value)
-			}
-			nn.layers[layer].activation(nn, layer)
-		}
-		// Output
-		var output []float64
-		for neuron := 0; neuron < nn.architecture[len(nn.architecture)-1]; neuron++ {
-			output = append(output, nn.layers[len(nn.architecture)-1].neurons[neuron].output)
-		}
-		outputs = append(outputs, output)
+		setInput(nn, inputs, sample)
+		feedforwardSample(nn)
+		outputs = append(outputs, getOutput(nn))
 	}
 	return
 }
 
+// train trains the network & saves the model
 func train(nn neuralNetwork, train_set [][]float64, test_set [][]float64, flagE bool) {
 	fmt.Printf("\n%v%vTrain model%v\n\n", BRIGHT, UNDERLINE, RESET)
 	for epoch := 1; epoch <= nn.epochs; epoch++ {

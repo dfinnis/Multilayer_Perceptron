a49ada942294ca2980bbc50259533586d644fc3a
Drew Finnis <drewfinnis@gmail.com>
2021-04-30 15:19:04 +0200

    refacto/test prints whats happening

--- test.sh
+++ test.sh
@@ -2,16 +2,28 @@
 # The following commands are the same as running:
 # go run main.go
 
+#### -- Print Header -- ####
+RESET="\x1b[0m"
+BRIGHT="\x1b[1m"
+
+printf "\E[H\E[2J" ## Clear screen
+printf $BRIGHT
+echo "Launching Multilayer Perceptron Test...$RESET\n"
+
 ## Create Datasets
+echo "Running evaluation.py to create data_training.csv and data_test.csv...\n"
 python3 evaluation.py
 
 ## Build
 go build
 
 ## Train
-./Multilayer_Perceptron data_training.csv -t -e
+echo "Training on data_training.csv...\n"
+./Multilayer_Perceptron data_training.csv -t
 
 ## Predict
+echo
+echo "Predicting on data_test.csv...\n"
 ./Multilayer_Perceptron data_test.csv -p
 
 ## Cleanup

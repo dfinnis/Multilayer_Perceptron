516fdeaf41c8437a5dbd85c0db06d4b03109bddf
Drew Finnis <drewfinnis@gmail.com>
2021-05-04 11:11:16 +0200

    featuee/relu & leaky relu activation, not integrated yet

--- src/activation.go
+++ src/activation.go
@@ -2,6 +2,14 @@ package multilayer
 
 import "math"
 
+// max32 returns the maximum value from 2 inputs
+func max32(max, value float32) float32 {
+	if max > value {
+		return max
+	}
+	return value
+}
+
 // sigmoid returns activation for one input
 func sigmoid(z float32) float32 {
 	return 1 / (1 + float32(math.Exp(float64(-z))))
@@ -25,14 +33,6 @@ func sigmoidPrime(inputs [][]float32) [][]float32 {
 		}
 	}
 	return outputs
-}
-
-// max32 reuturns the max from 2 inputs
-func max32(max, value float32) float32 {
-	if max > value {
-		return max
-	}
-	return value
 }
 
 // softmax returns activation for a layer
@@ -79,4 +79,75 @@ func softmaxPrime(z [][]float32) (d_Z [][]float32) {
 		d_Z = append(d_Z, d)
 	}
 	return
+}
+
+// relu - Rectified Linear Unit, returns activation for one input
+func relu(z float32) float32 {
+	return max32(0, z)
+}
+
+// reluLayer applies relu activation to each neuron's value, saving output
+func reluLayer(nn neuralNetwork, layer int) {
+	for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
+		nn.layers[layer].neurons[neuron].output = relu(nn.layers[layer].neurons[neuron].value)
+		nn.layers[layer].neurons[neuron].outputs = append(nn.layers[layer].neurons[neuron].outputs, nn.layers[layer].neurons[neuron].output)
+	}
+}
+
+// reluPrime - Rectified Linear Unit, returns activation derivative for one input
+func reluPrime(z float32) float32 {
+	if z > 0 {
+		return 1
+	}
+	return 0
+}
+
+// reluPrime - Rectified Linear Unit, returns activation derivative for one layer
+func reluPrimeLayer(inputs [][]float32) [][]float32 {
+	outputs := make([][]float32, len(inputs))
+	for i, input := range inputs {
+		outputs[i] = make([]float32, len(inputs[0]))
+		for j, z := range input {
+			outputs[i][j] = reluPrime(z)
+		}
+	}
+	return outputs
+}
+
+// leakyRelu - Leaky Rectified Linear Unit, returns activation for one input
+func leakyRelu(z float32) float32 {
+	var alpha float32 = 0.1
+	if z >= 0 {
+		return z
+	}
+	return z * alpha
+}
+
+// leakyRelu applies leakyRelu activation to each neuron's value, saving output
+func leakyReluLayer(nn neuralNetwork, layer int) {
+	for neuron := 0; neuron < nn.architecture[layer]; neuron++ {
+		nn.layers[layer].neurons[neuron].output = leakyRelu(nn.layers[layer].neurons[neuron].value)
+		nn.layers[layer].neurons[neuron].outputs = append(nn.layers[layer].neurons[neuron].outputs, nn.layers[layer].neurons[neuron].output)
+	}
+}
+
+// leakyReluPrime - Leaky Rectified Linear Unit, returns activation derivative for one input
+func leakyReluPrime(z float32) float32 {
+	var alpha float32 = 0.1
+	if z >= 0 {
+		return 1
+	}
+	return alpha
+}
+
+// leakyReluPrime - Leaky Rectified Linear Unit, returns activation derivative for one layer
+func leakyReluPrimeLayer(inputs [][]float32) [][]float32 {
+	outputs := make([][]float32, len(inputs))
+	for i, input := range inputs {
+		outputs[i] = make([]float32, len(inputs[0]))
+		for j, z := range input {
+			outputs[i][j] = leakyReluPrime(z)
+		}
+	}
+	return outputs
 }

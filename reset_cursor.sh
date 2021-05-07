f0f12b31df328eebe88e28857116217c708a85a0
Drew Finnis <drewfinnis@gmail.com>
2021-05-07 20:23:27 +0200

    refacto/corewar to Mutilayer

--- reset_cursor.sh
+++ reset_cursor.sh
@@ -1,3 +1,4 @@
 #### -- reset cursor -- ####
-## If corewar is killed, you may need to reset your cursor
+## The cursor is hidden while training for clean output
+## If Multilayer_Perceptron is killed, you may need to reset your cursor
 echo "\033[?12;25h"

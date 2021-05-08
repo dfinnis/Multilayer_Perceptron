01f5c66be9ea74b214b05a9cbf6975fa69690def
Drew Finnis <drewfinnis@gmail.com>
2021-05-08 13:06:20 +0200

    refacto/order usage

--- src/error.go
+++ src/error.go
@@ -7,14 +7,14 @@ import (
 
 // printUsage prints usage & quits
 func printUsage() {
-	fmt.Printf("\nUsage:\tgo build; ./Multilayer_Perceptron [DATA.CSV] [-t] [-p [FILEPATH]] [-e] [-ep EPOCHS] [-s SEED] [-a ARCHITECTURE] [-l LEARNING] [-q]\n\n")
+	fmt.Printf("\nUsage:\tgo build; ./Multilayer_Perceptron [DATA.CSV] [-t] [-p [FILEPATH]] [-s SEED] [-e] [-ep EPOCHS] [-l LEARNING] [-a ARCHITECTURE] [-q] [-h]\n\n")
 	fmt.Printf("    [-t]  (--train)        Only train, don't predict. Overwrites existing model\n")
 	fmt.Printf("    [-p]  (--predict)      Only predict, don't train. Optional [FILEPATH] load model from filepath\n")
+	fmt.Printf("    [-s]  (--seed)         Provide SEED integer for randomization e.g. -s 42\n")
 	fmt.Printf("    [-e]  (--early)        Early stopping. Stop training when test set loss starts increasing\n")
 	fmt.Printf("    [-ep] (--epochs)       Provide EPOCHS. Must be integer between 0 & 100000, default 15000\n")
-	fmt.Printf("    [-s]  (--seed)         Provide SEED integer for randomization e.g. -s 42\n")
-	fmt.Printf("    [-a]  (--architecture) Provide ARCHITECTURE as string e.g. -a \"16 16 2\"\n")
 	fmt.Printf("    [-l]  (--learning)     Provide LEARNING rate. Must be float between 0 & 1, default 0.01\n")
+	fmt.Printf("    [-a]  (--architecture) Provide ARCHITECTURE as string e.g. -a \"16 16 2\"\n")
 	fmt.Printf("    [-q]  (--quiet)        Don't print architecture or seed\n")
 	fmt.Printf("    [-h]  (--help)         Show usage\n\n")
 	os.Exit(1)
